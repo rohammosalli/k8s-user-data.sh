13dc6e14072ced8bda5818dc4a357ed3c00965ed
Daniel Gordi <danial@gordi.ir>
2018-11-30 13:55:00 +0330

    Add sudo to apt command and sudo check

--- k8s-user-data.sh
+++ k8s-user-data.sh
@@ -1,25 +1,30 @@
-#!/bin/bash 
+#!/bin/bash
 
-apt-get update
-apt-get install -y git wget
+if [ "$(whoami)" != "root" ]; then
+        echo "Sorry, You must run this script with root privileges"
+        exit 1
+fi
+
+sudo apt-get update
+sudo apt-get install -y git wget
 
 # Install Docker
-apt-get install \
+sudo apt-get install \
     apt-transport-https \
     ca-certificates \
     curl \
     software-properties-common
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-apt-key fingerprint 0EBFCD88
-add-apt-repository \
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
-   
-apt-get update
 
-apt-get install -yq docker-ce
+sudo apt-get update
+
+sudo apt-get install -yq docker-ce
 
 
 
